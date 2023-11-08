8a7d5ed2ead1b83551fe993ab78238b810004510
ad.hanif <ad.hanif@outlook.com>
2023-11-08 12:55:28 +0100

    hero section completed

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -5,7 +5,7 @@ import heroImg from "../assets/hero.jpg";
 const HeroSection = () => {
   return (
     <div
-      className="relative overflow-hidden bg-no-repeat bg-center  h-[700px] object-fill bg-opacity-50"
+      className="relative overflow-hidden bg-no-repeat bg-center md:h-[600px] lg:h-[700px] object-fill bg-opacity-50"
       style={{
         backgroundImage: `url(${heroImg})`,
       }}
@@ -14,31 +14,34 @@ const HeroSection = () => {
         className=" h-[700px] bg-opacity-50 "
         style={{ backgroundColor: "rgba(0, 0, 0, 0.65)" }}
       >
-        <div className="container flex flex-col sm:flex-row justify-between mx-auto items-center px-28 py-6">
+        <div className=" flex flex-row justify-between  items-center px-10 xl:px-28 py-6  mx-0 xl:mx-14 ">
           <div>
-            <img src={NetflixLogo} alt="" className="h-10" />
+            <img
+              src={NetflixLogo}
+              alt=""
+              className="h-[1.8rem] w-[6rem]   lg:h-[2.5rem] lg:w-[10rem]"
+            />
           </div>
 
-          <div className="flex items-center  space-x-0 sm:space-x-6 flex-col md:flex-row">
+          <div className="flex  items-center  space-x-2 lg:space-x-6  ">
             <div>
               <select
-                className="border rounded px-6 py-1 font-medium "
+                className="border rounded  sm:px-6 py-[5px] sm:py-[3px] font-medium text-sm sm:text-base"
                 style={{ backgroundColor: "rgba(0, 0, 0, 0.8)", color: "#fff" }}
               >
                 <option>English</option>
                 <option>Deutsch</option>
-                {/* <option>Maybe</option> */}
               </select>
             </div>
             <button
               type="button"
-              className="text-sm font-medium text-white bg-netCol hover:bg-netColHover px-[17px] py-1.5 rounded cursor-pointer"
+              className="text-sm font-bold text-white bg-netCol hover:bg-netColHover px-[1rem] sm:px-[17px] py-1.5 rounded cursor-pointer "
             >
               Sign In
             </button>
           </div>
         </div>
-        <div className="container mx-auto text-center md: mt-24 lg:mt-48 ">
+        <div className="container mx-auto text-center mt-[4rem] md:mt-24 lg:mt-48 ">
           <p className="    text-3xl   lg:text-5xl font-custom font-black text-white px-2 ">
             Unlimited movies, TV shows, and more
           </p>
@@ -52,9 +55,8 @@ const HeroSection = () => {
           <div className="container mx-auto flex justify-center space-x-0 sm:space-x-3 flex-col sm:flex-row px-5 sm:px-0">
             <input
               type="text"
-              className="rounded px-6 py-[0.8rem] font-medium  md:w-[23rem] bg-opacity-50 bg-zinc-900 "
+              className="rounded px-6 py-[0.8rem] font-medium  md:w-[23rem] bg-opacity-50 bg-zinc-900 border-[1px] text-white"
               placeholder="Email address"
-              //   style={{ backgroundColor: "rgba(0, 0, 0, 0.8)", color: "#fff" }}
             />
             <button
               type="button"
