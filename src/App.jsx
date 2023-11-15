b916518f2a5bfb64113e7ddea6eea57c18c33341
ad.hanif <ad.hanif@outlook.com>
2023-11-15 23:09:13 +0100

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Home from "./component/Home";
 import SignIn from "./component/SignIn";
 import SignUp from "./component/SignUp";
+import Footer from "./Footer";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Footer />
     </>
   );
 }
