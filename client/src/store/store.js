7897c0794a82eb17a5b4316e204ef22420898fd6
ad.hanif <ad.hanif@outlook.com>
2023-12-11 15:53:30 +0100

    configred redux store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../reducers/userSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    users: userReducer,
+  },
 });
 
 export default store;
